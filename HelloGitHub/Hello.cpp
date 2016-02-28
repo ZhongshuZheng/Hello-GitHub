2998652e266a89a21e0c0e0da44e9306003a2d3f
ZhongshuZheng <354681250@qq.com>
2016-02-28 14:36:43 +0800

    Added files via upload

---
+++ HelloGitHub/Hello.cpp
@@ -0,0 +1,7 @@
+#include"stdio.h"
+
+int main()
+{
+    print("Hello GitHub!");
+    return 0;
+}
---
+++ HelloGitHub/Hello.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main extends Exception
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello GitHub!");
+    }
+}
